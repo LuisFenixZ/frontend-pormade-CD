8871be4e31aebc837ddf8a0a3dbf8e8f862a8774
Luis Fenix Domborovski <luisfenixdomborovski@gmail.com>
2023-08-21 10:06:25 -0300

    center-content-payment

--- src/pages/mob-pixqrcode/payment.js
+++ src/pages/mob-pixqrcode/payment.js
@@ -10,6 +10,7 @@ const Payment =() => {
                 <p className="info_text">Crachá:</p>
                 <p className="info_text">Valor: R$</p>
                 <img src= {QrCodePix} className="pix_qrcode"></img>
+                <button className="button_confirm">Confirmar</button>
 
         </div>
     );

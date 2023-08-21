9e7ec8c33f91b4b7d7b791a1ac297e9c1f15ea48
Denielkas <132719249+Denielkas@users.noreply.github.com>
2023-08-21 10:03:09 -0300

    home

--- src/pages/mob-company/choose-company.js
+++ src/pages/mob-company/choose-company.js
@@ -8,6 +8,9 @@ const Company =() => {
         <h1 className="title">Cantina da 
             Confiança</h1>
             <h3 className="subtitle">Selecione a empresa</h3>
+            <a className="anchor">Pormade</a>
+            <a className="anchor">DRZ</a>
+            <img className="img-logo" src="https://cdn.discordapp.com/attachments/1141361461516587098/1142095072578506842/Cantina_Logo_White_WLP.png"></img>
         </div>
     );
 };
--- src/pages/mob-pixqrcode/payment.js
+++ src/pages/mob-pixqrcode/payment.js
@@ -5,6 +5,7 @@ import QrCodePix from "../../../src/img-source/qrcode-pix/Qr Pix Cantina White.p
 const Payment =() => {
     return(
         <div className="container">
+            <a className="home" href="https://www.figma.com/file/r7MfVKvQXP3fIA1ES0ZwNw/Pormade-CD?type=design&node-id=0-1&mode=design&t=bGd2KrkfEwl4xU0Z-0"><img src="https://cdn.discordapp.com/attachments/1141361461516587098/1143165039143034911/icone-de-la-maison-verte.png"></img></a>
             <h1 className="title">Cantina da Confiança</h1>
                 <p className="info_text">Nome:</p>
                 <p className="info_text">Crachá:</p>

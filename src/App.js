8081d1de5f0600e292fb3c5ecd111d1db0b2ebfe
Denielkas <132719249+Denielkas@users.noreply.github.com>
2023-08-22 11:00:18 -0300

    telas

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './index.css';
 import Login from "./pages/login-page/Login";
 import Payment from './pages/mob-pixqrcode/payment';
 import Company from "./pages/mob-company/choose-company";
+import Choice from "./pages/choice/choice";
+import Report from "./pages/generate-report/generate-report";
 
 
 
@@ -19,6 +21,10 @@ function App() {
         <Route path="/company" element={<Company/>}></Route>
 
         <Route path="/payment" element={<Payment/>}></Route>
+
+        <Route path="/choice" element={<Choice/>}></Route>
+
+        <Route path="/report" element={<Report/>}></Route>
 
       </Routes>
     </Router>

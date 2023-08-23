475309b80f317c4ec98672d52f9204919cb1822e
Denielkas <132719249+Denielkas@users.noreply.github.com>
2023-08-23 12:26:14 -0300

    tela inicial

--- src/pages/mob-company/choose-company.js
+++ src/pages/mob-company/choose-company.js
@@ -1,20 +1,22 @@
 import React from "react";
 import './choose-company.css';
 import Cantina from "../../img-source/logo-cantina/Cantina Logo PetimColor WLP 1.png";
-
-
+import Clock from "./clock";
 
 const Company =() => {
     return(
-        <div className="container-box">
+        <div className="container-box borda-animada">
         <h1 className="title">Cantina da 
             Confiança</h1>
-            <h3 className="subtitle">Selecione a empresa</h3>
-            <a className="anchor" href="#"><p className="text">Pormade</p></a>
-            <a className="anchor" href="#"><p className="text">DRZ</p></a>
+            <h3 id="hora" className="clock"></h3>
+            <a className="anchor" href="#"><p className="text-play">Começar</p></a>
             <img className="img-logo" src={Cantina}></img>
         </div>
     );
 };
+
+
+
+
 
 export default Company;  

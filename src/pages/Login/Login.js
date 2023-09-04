7d1cedbc9add37f1d933103ed99ab92060579e84
lucasbaumgardt <142561750+lucasbaumgardt@users.noreply.github.com>
2023-09-04 01:39:29 -0300

    Atualização página de login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,69 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Swal from "sweetalert2";
 import './style-login.css';
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
-import api from "../../services/api";
 import logoCantina from "../../img/Cantina Logo White WLP.png";
+import api from "../../services/api";
 
 const Login = () => {
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [showRegisterPassword, setShowRegisterPassword] = useState(false);
-  const [is_admin, setIs_Admin] = useState(false);
-
   const history = useNavigate();
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      setAuthenticated(true);
+      history.replace("/admin");
+    }
+  }, []);
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-     
-      const response = await api.post("/users/login", {
-        email: loginEmail,
-        password: loginPassword,
-      });
 
-      if (response.status === 200) {
-        const {is_admin} = response.data;
-        if (is_admin) {
-          history('/admin');
-          Swal.fire("Login Realizado com Sucesso", "Seja Bem Vindo!", "success")
-        } 
-        else {
-          history('/mostra-produtos2');
-          Swal.fire("Login Realizado com Sucesso", "Seja Bem Vindo!", "success")
-        }
-        
+      console.log(login);
+      console.log(password);
+
+      const response = await api.post("/users/validate", {
+        login: login,
+        password: password,
+      });
+      console.log(response);
+
+      
+      const { accessToken } = response.data;
+
+      if (accessToken) {
+        setAuthenticated(true);
+        console.log("Antes de navegar para /admin");
+        history("/admin");
+        console.log("Após navegar para /admin");
+      
+        Swal.fire("Login Realizado com Sucesso", "Seja Bem Vindo!", "success");
+      } else {
+        Swal.fire("Erro ao fazer login", "Verifique suas credenciais e tente novamente", "error");
       }
+        
+      
     } catch (error) {
-      console.log(error);
+      console.error("Erro ao executar a função handleLogin:", error);
       Swal.fire("Erro ao fazer login", "Verifique suas credenciais e tente novamente", "error");
     }
   };
 
-  const handleRegister = async (e) => {
-    e.preventDefault();
-    try {
-  
-      const response = await api.post("/users/register", {
-        email: registerEmail,
-        password: registerPassword,
-        is_admin: is_admin,
-      });
-
-      if (response.status === 200) {
-        Swal.fire("Registro bem-sucedido", "Faça login para continuar", "success");
-        setRegisterEmail("");
-        setRegisterPassword("");
-      }
-    } catch (error) {
-      console.log(error);
-      Swal.fire("Erro ao registrar", "Verifique se o email já está cadastrado", "error");
-    }
-  };
+  if (authenticated) {
+    return <div>Você está autenticado!</div>;
+  }
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -81,7 +76,7 @@ const Login = () => {
       
                 <p className="par">Usuário</p>
                 <div className="input">    
-                    <input type="text" name="emailLogin" value={loginEmail} onChange={e => setLoginEmail(e.target.value)} className="inputCamp"  />
+                    <input type="text" name="emailLogin" value={login} onChange={e => setLogin(e.target.value)} className="inputCamp"  />
                 </div>
               
                 <p className="par">Senha</p>
@@ -90,8 +85,8 @@ const Login = () => {
                     <div className="select__options__category">
 
                     <input type={showPassword ? "text" : "password"} name="passwordLogin" 
-                    value={loginPassword} 
-                    onChange={e => setLoginPassword(e.target.value)} 
+                    value={password} 
+                    onChange={e => setPassword(e.target.value)} 
                     className="inputCamp"/>
 
                     </div>

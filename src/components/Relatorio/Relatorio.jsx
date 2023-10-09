8ee4d888fca0a83645643a87c662c903520a3f47
lucasbaumgardt <142561750+lucasbaumgardt@users.noreply.github.com>
2023-10-09 02:29:26 -0300

    Adição das funções baixar PDF e Excel

--- src/components/Relatorio/Relatorio.jsx
+++ src/components/Relatorio/Relatorio.jsx
@@ -1,17 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import * as pdfMake from 'pdfmake/build/pdfmake';
-import 'pdfmake/build/vfs_fonts';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 import './relatorio.css';
 import api from '../../services/api';
-pdfMake.vfs = pdfMake.vfs;
+
 import Calendario from './Calendario'; // Importe o componente Calendario aqui
 
+pdfMake.fonts = {
+  Roboto: {
+    normal: 'Roboto-Regular.ttf',
+    bold: 'Roboto-Medium.ttf',
+    italics: 'Roboto-Italic.ttf',
+    bolditalics: 'Roboto-MediumItalic.ttf',
+  },
+};
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const Relatorio = () => {
 
   const [purchases, setPurchases] = useState([]);
   const [month, setMonth] = useState(new Date().getMonth() + 1); // Mês atual como valor padrão
   const [year, setYear] = useState(new Date().getFullYear()); // Ano atual como valor padrão
+  const [pdfSrc, setPdfSrc] = useState(null);
+  const [isPdfOpen, setIsPdfOpen] = useState(false);
 
   async function fetchData() {
     try {
@@ -116,8 +128,76 @@ const Relatorio = () => {
       },
     };
   
-    pdfMake.createPdf(docDefinition).open(); // Abre o PDF em uma nova guia
+    pdfMake.createPdf(docDefinition).open(''); // Abre o PDF em uma nova guia
+    pdfMake.createPdf(docDefinition).download(`relatórioCantina${months.find(m => m.value === month)?.name}`);
     // pdfMake.createPdf(docDefinition).download('relatorio.pdf'); // Faz o download do PDF com o nome "relatorio.pdf"
+  };
+
+  const viewPdf = () => {
+    const docDefinition = {
+      content: [
+        {
+          text: `Relatório de Compras do mês de ${months.find(m => m.value === month)?.name}`,
+          style: 'header',
+        },
+        {
+          table: {
+            headerRows: 1,
+            widths: ['auto', 'auto', 'auto', 'auto', 'auto', 'auto'],
+            body: [
+              ['ID', 'Valor', 'Nome do Colaborador', 'CPF do Colaborador', 'Crachá', 'Data - Hora'],
+              ...purchases.map((purchase) => [
+                purchase.id,
+                purchase.value ? (
+                  parseFloat(purchase.value.replace('$', '').replace(',', '.')).toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  })
+                ) : '',
+                purchase.customerName,
+                purchase.customerCpf,
+                purchase.customerBadge,
+                formatDateTime(purchase.date),
+              ]),
+              ['Total', purchases.length > 0 && purchases[0].totalValue ? (
+                purchases[0].totalValue.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              ) : '', '', '', '', '']
+            ],
+          },
+          layout: 'lightHorizontalLines',
+        },
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          alignment: 'center',
+          margin: [0, 0, 0, 20],
+        },
+      },
+    };
+
+    const pdfDoc = pdfMake.createPdf(docDefinition);
+
+    if (isPdfOpen) {
+      closePdf();
+    } else {
+      const pdfDoc = pdfMake.createPdf(docDefinition);
+      pdfDoc.getBlob((blob) => {
+        const objectURL = URL.createObjectURL(blob);
+        setPdfSrc(objectURL);
+        setIsPdfOpen(true);
+      });
+    }
+
+  };
+
+  const closePdf = () => {
+    setPdfSrc(null);
+    setIsPdfOpen(false);
   };
   
   const handleCalendarChange = (selectedMonth, selectedYear) => {
@@ -140,6 +220,35 @@ const Relatorio = () => {
     { value: 12, name: 'Dezembro' },
   ];
 
+  const downloadExcelSheet = async () => {
+    const adminToken = localStorage.getItem('adminToken');
+
+    try {
+      const response = await api.get(`/purchases/sheet/month=${month}/year=${year}`, {
+        responseType: 'blob', // Indica que a resposta é um blob (arquivo)
+        headers: {
+          Authorization: `Bearer ${adminToken}`, // Inclua o token de administração no cabeçalho
+        },
+      });
+  
+      if (response.status === 200) {
+        const blob = new Blob([response.data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = `relatórioCantina${months.find(m => m.value === month)?.name}.xlsx`; // Especifique o nome do arquivo aqui
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+      } else {
+        console.error('Falha ao baixar a planilha.');
+      }
+    } catch (error) {
+      console.error('Erro ao fazer a solicitação:', error);
+    }
+  };
+  
+
   return (
     <div className='container2'>
       <h1 className="h1_table_title">Registro de compras {months.find(m => m.value === month)?.name} de {year}</h1>
@@ -149,7 +258,9 @@ const Relatorio = () => {
           <Calendario onChange={handleCalendarChange} />
         </div>
         <div>
-          <button className="relatorio" onClick={generatePdf}>Ver em PDF</button>
+          {/* <button className="relatorio" onClick={viewPdf}>{isPdfOpen ? 'Fechar Visualização' : 'Visualizar PDF'}</button> */}
+          <button className="download" onClick={generatePdf}>Download PDF</button>
+          <button className="download2" onClick={downloadExcelSheet}>Download Excel</button>
         </div>
 
       </div>
@@ -210,7 +321,7 @@ const Relatorio = () => {
           </thead>
         </table>
       </div>
-
+      {/* {pdfSrc && <iframe src={pdfSrc} title="PDF Viewer" className='pdf_iframe'></iframe>} */}
     </div>
   );
 };

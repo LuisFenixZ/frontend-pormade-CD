3523da8ee9de7b456931aabbb4eda8381b50d1a2
lucasbaumgardt <142561750+lucasbaumgardt@users.noreply.github.com>
2024-01-05 13:22:37 -0300

    Correções menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,63 +1,94 @@
 import React, { useState, useContext } from 'react';
+import Modal from 'react-modal';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/useAuth';
 import { TbMenu2 } from "react-icons/tb";
 import logoPormade from '../../img/logoPormade.png';
-import "./styles.css";
-
 
 function NavBar(props) {
   
   const { singOut } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
-  // const [userInfoVisibility, setUserInfoVisibility] = useState(false);
+    setModalIsOpen(true);
+  }
   
   const handleLogout = () => {
     singOut();
     navigate('/login');
-    setIsModalOpen(false);
+    setModalIsOpen(false);
   };
 
-  return (
-    <nav className=''>
+  const customStyles = {
+    content: {
+      position: 'fixed',
+      top: '50%',
+      left: '100%', 
+      width: '50%',
+      height: '100vh',
+      transform: 'translate(-50%, -50%)',
+      backgroundColor: '#0A0A0A',
+      border: 'none',
+      borderLeft: '2px solid #509D46',
+      borderRadius: '15px',
+      padding: '20px',
+      boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+      zIndex: 1000,
+      overflowY: 'hidden'
+    },
+    overlay: {
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100vh',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      zIndex: 999,
+    },
+  };
+  
 
-      <div className='div_logout'>
+  return (
+    <nav className='flex justify-around items-center h-[65px] p-[0 1%] bg-grey1 text-white sticky top-0 z-10'>
+
+      <div className='w-[100%]'>
     
       </div>
 
-      <div className='div_center_nav'>
-      <img src={logoPormade} alt='logoPormade' className='nav-home-return'></img>
+      <div className='w-[100%] flex flex-row justify-center items-center'>
+      <img src={logoPormade} alt='logoPormade' className='mr-auto ml-auto w-[35px] h-[35px] text-green2'></img>
       </div>
 
-      <div className='div_cart_menu'>
-        <TbMenu2 className='menu_nav' onClick={handleOpenModal}></TbMenu2>
+      <div className='w-[100%] m-5'>
+        <TbMenu2 className='w-[35px] h-[35px] text-white cursor-pointer float-right' onClick={handleOpenModal}></TbMenu2>
       </div>
 
-      {isModalOpen && ( 
-      <div className="modal">
-        <div className="modal-content">
-          <div className='modal_titulo'>
+      <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={() => setModalIsOpen(false)}
+      contentLabel="Carrinho de Compras"
+      style={customStyles}
+      >
+        
+          <div className='w-[100%] h-[65px] flex flex-row justify-center items-center'>
+            <div className='flex flex-row justify-center items-center'>
+              <TbMenu2 className='w-[35px] h-[35px] text-white cursor-pointer' onClick={handleOpenModal}></TbMenu2>
+            </div>
+            
+          </div>
+
+          <div className=''>
           <p>Conteúdo do Menu</p>
-            <TbMenu2 className='menu_nav_close' onClick={handleCloseModal}>&times;</TbMenu2>
           </div>
 
           <div>
             <button onClick={handleLogout}>Logout</button>  
           </div> 
-        </div>
-      </div>
-    )}
+          
+      </Modal>
     
     </nav>
   );
